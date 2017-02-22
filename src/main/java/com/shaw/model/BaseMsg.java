27147e4519e982aacd66371d87d0dd7a0da172c5
imn5100 <imn5100_chu2@163.com>
2017-02-22 20:03:56 +0800

    build base chat msg

---
+++ src/main/java/com/shaw/model/BaseMsg.java
@@ -0,0 +1,25 @@
+package com.shaw.model;
+
+/**
+ * Created by shaw on 2017/2/22 0022.
+ */
+public class BaseMsg {
+    private String type;
+    private String contents;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
--- src/main/java/com/shaw/websocket/assembly/MessageWebSocketHandler.java
+++ src/main/java/com/shaw/websocket/assembly/MessageWebSocketHandler.java
@@ -1,11 +1,15 @@
 package com.shaw.websocket.assembly;
 
+import com.alibaba.fastjson.JSON;
+import com.shaw.model.BaseMsg;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,38 +19,71 @@ import java.util.Set;
 public class MessageWebSocketHandler extends AbstractWebSocketHandler {
 
     //写时复制，保证现场安全。set结构保证同一个会话只存储一份
-    Set<WebSocketSession> sessionList = new java.util.concurrent.CopyOnWriteArraySet<>();
-//    @Autowired
-//    ParticipantRepository participantRepository;
+    Set<WebSocketSession> sessionSet = new java.util.concurrent.CopyOnWriteArraySet<>();
+    //映射 id和 用户名（用户信息）
+    Map<String, String> idNameMap = new java.util.concurrent.ConcurrentHashMap<String, String>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
-        sessionList.add(webSocketSession);
-        System.out.println("afterConnectionEstablished");
     }
 
 
     @Override
     public void handleTextMessage(WebSocketSession webSocketSession, TextMessage webSocketMessage) throws Exception {
-        TextMessage myMessage = new TextMessage("I:" + webSocketMessage.getPayload());
-        TextMessage otherMessage = new TextMessage(webSocketSession.getId() + ":" + webSocketMessage.getPayload());
-        for (WebSocketSession session : sessionList) {
-            if (session != null && webSocketSession != session) {
-                session.sendMessage(otherMessage);
+        String msg = webSocketMessage.getPayload();
+        if (StringUtils.isEmpty(msg)) {
+            return;
+        } else if (msg.startsWith("{")) {
+            try {
+                BaseMsg getMsg = JSON.parseObject(msg, BaseMsg.class);
+                if (StringUtils.isEmpty(getMsg.getType()) || StringUtils.isEmpty(getMsg.getContents())) {
+                    return;
+                } else {
+                    if (getMsg.getType().equalsIgnoreCase("login")) {
+                        sessionSet.add(webSocketSession);
+                        idNameMap.put(webSocketSession.getId(), getMsg.getContents());
+                        int userNum = sessionSet.size();
+                        BaseMsg countSendMsg = new BaseMsg();
+                        countSendMsg.setType("count");
+                        countSendMsg.setContents(String.valueOf(userNum));
+                        BaseMsg sendMsg = new BaseMsg();
+                        sendMsg.setType("msg");
+                        sendMsg.setContents(getMsg.getContents() + " is Online!");
+                        String countSendMsgStr = JSON.toJSONString(countSendMsg);
+                        String sendMsgStr = JSON.toJSONString(sendMsg);
+                        TextMessage sendTextMessage = new TextMessage(sendMsgStr);
+                        TextMessage countSendTextMessage = new TextMessage(countSendMsgStr);
+                        for (WebSocketSession session : sessionSet) {
+                            session.sendMessage(countSendTextMessage);
+                            session.sendMessage(sendTextMessage);
+                        }
+                    } else if (getMsg.getType().equalsIgnoreCase("msg")) {
+                        String myName = idNameMap.get(webSocketSession.getId());
+                        BaseMsg sendMsg = new BaseMsg();
+                        sendMsg.setType("msg");
+                        for (WebSocketSession session : sessionSet) {
+                            sendMsg.setContents(myName + ":" + getMsg.getContents());
+                            session.sendMessage(new TextMessage(JSON.toJSONString(sendMsg)));
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                webSocketSession.close();
             }
         }
-        webSocketSession.sendMessage(myMessage);
     }
 
     @Override
     public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) throws Exception {
-        System.out.println("handleTransportError");
+        sessionSet.remove(webSocketSession);
+        idNameMap.remove(webSocketSession.getId());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception {
-        System.out.println("afterConnectionClosed");
-        sessionList.remove(webSocketSession);
+        sessionSet.remove(webSocketSession);
+        idNameMap.remove(webSocketSession.getId());
     }
 
     @Override
