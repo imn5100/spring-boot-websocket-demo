4a3db37f775e8bae6f873128b1dc0cfe0d3b3a9a
xiaowc <xiaowc@dxy.cn>
2017-04-01 18:39:42 +0800

    update

--- src/main/java/com/shaw/websocket/assembly/MessageWebSocketInterceptor.java
+++ src/main/java/com/shaw/websocket/assembly/MessageWebSocketInterceptor.java
@@ -1,7 +1,6 @@
 package com.shaw.websocket.assembly;
 
 import com.shaw.constants.Utils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -22,16 +21,14 @@ public class MessageWebSocketInterceptor extends HttpSessionHandshakeInterceptor
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
         if (request instanceof ServletServerHttpRequest) {
             ServletServerHttpRequest serverRequest = (ServletServerHttpRequest) request;
-            String encoded = Utils.getCookieValue(serverRequest.getServletRequest(), COOKIE_NAME);
-            if (StringUtils.isNotEmpty(encoded)) {
-                Map<String, Object> map = Utils.getParams(encoded);
-                for (Map.Entry entry : map.entrySet()) {
-                    System.out.println(entry.getKey() + ":" + entry.getValue());
-                }
+            Map<String, Object> map = Utils.getParamsFromCookie(serverRequest.getServletRequest(), COOKIE_NAME);
+            for (Map.Entry entry : map.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue());
             }
         }
         return super.beforeHandshake(request, response, wsHandler, attributes);
     }
+
 
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception e) {
